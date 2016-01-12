4d4da3b8e89dc5b6eb1ff4625d491fd0e7fae87e
lafranli <lucie.lafrance@etudiant.univ-rennes1.fr>
2016-01-12 11:36:24 +0100

    Create header.php

---
+++ application/views/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+	
+	<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+		<head>
+			<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+			<title> TODO LIST </title>
+			<link rel="stylesheet" type="text/css" href="../../assets/styles/base.css" media="all" />
+			<link rel="stylesheet" type="text/css" href="../../assets/styles/modele03.css" media="screen" />
+		</head>
+
+		<body>
+
+			<div id="global">
+
+			<div id="entete">
+
+				<h1> Web Platform de Projet Trans
+				</h1>
+
+				<p class="sous-titre">
+					<strong>Caractéristiques:</strong>
+				platform web de réservation de salle
+				</p>
+			</div><!-- #entete -->
+			<div id="navigation">
+				<ul>
+					<li><a href="./connexion.php" Connexion</a></li>
+				</ul>
+			</div><!-- #navigation -->

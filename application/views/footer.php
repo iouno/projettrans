9c74f7721961c7a3a934403b7a19e14f686cf0dd
lafranli <lucie.lafrance@etudiant.univ-rennes1.fr>
2016-01-14 10:33:03 +0100

    Update footer.php

---
+++ application/views/footer.php
@@ -0,0 +1,16 @@
+    
+<?php
+			echo "<div id=\"pied\">
+				<p> Interface de réservation de salle de concert pour les Artistes des TransMusicales
+				</p>
+				</br>
+				<p> Projet <strong>&copy; 2015-2016 </strong> IUT de Lannion
+				</p>
+			</div><!-- #pied -->
+
+		</div><!-- #global -->
+
+	</body>
+</html>";
+
+?>

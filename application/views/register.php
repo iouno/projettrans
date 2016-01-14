1088a5b65e75537cc725d59d5c5008703c8ea91f
Nolwenn Fournier <nfournier29@gmail.com>
2016-01-14 12:05:30 +0100

    Suppression des fichiers models

--- application/views/register.php
+++ application/views/register.php
@@ -4,8 +4,8 @@
 <form method="post" action="<?php $_SERVER['PHP_SELF']?>" enctype="multipart/form-data">
 	<fieldset>
 	<label for="nom">*Nom :</label> <input name="nom" type="text" id="nom"/> (le nom doit contenir entre 3 et 50 caractères) </br>
-	<label for="mail">*Mail :</label> <input name="mail" type="mail" id="mail"/> </br>
-	<label for="pays">*Pays :</label> <input name="pays" type="text" id="pays"/> </br>
+	<label for="mail">*Mail :</label> <input name="mail" type="mail" id="mail"/></br>
+	<label for="pays">*Pays :</label> <input name="pays" type="text" id="pays"/></br>
 	<label for="dateDeb">*Date de début :</label> <input name="dateDeb" type="text" id="dateDeb"/> </br>
 	<label for="formation">*Formation : </label> <input name="formation" type="text" id="formation"/> </br>
 	<label for="genre">*Genre : </label> <input name="genre" type="text" id="genre"/> </br>

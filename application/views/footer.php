5556d02d73838171bd7dcc3291b590ddae0423db
iouno <nfournier29@gmail.com>
2016-01-20 23:05:58 +0100

    Retouches du CSS

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,8 +1,7 @@
-    		</div>
+    	</section>
 		<footer>
 			<p> Interface de réservation de salle de concert pour les Artistes des TransMusicales</p>
 			<p> Projet <strong>&copy; 2015-2016 </strong> IUT de Lannion
 		</footer>
-
 	</body>
 </html>

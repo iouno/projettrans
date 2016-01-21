ca64830d2674fe9c3c8b8965bc1ea6623d463f9e
lafranli <lafranli@debian8.iutlan.univ-rennes1.fr>
2016-01-21 10:04:27 +0100

    modifié :         application/views/home.php 	assets/images/

--- application/views/home.php
+++ application/views/home.php
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-<?php echo anchor('artiste/inscription','inscrivez-vous') ?>
 
-=======
 <p><?php echo anchor('artiste/inscription','inscrivez-vous') ?></p>
 <p><?php echo anchor('artiste/recherche','faire une recherche') ?>(temp)</p>
->>>>>>> a25163a1c13849ffab492a7c165e0c64e1386531
+

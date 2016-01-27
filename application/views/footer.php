c7bd5e3f34c351c99998e5958310b53055477cb3
lafranli <lucie.lafrance@etudiant.univ-rennes1.fr>
2016-01-27 17:12:56 +0100

    Update footer.php

--- application/views/footer.php
+++ application/views/footer.php
@@ -2,7 +2,7 @@
 
 	    	</section>
 			<footer>
-				<p> <?php echo_("Interface de réservation de salle de concert pour les Artistes des TransMusicales"); ?></p>
+				<p> <?php echo gettext("Interface de réservation de salle de concert pour les Artistes des TransMusicales"); ?></p>
 				<p> Projet Trans <strong>&copy; 2015-2016 </strong> IUT de Lannion
 			</footer>
 		</div>

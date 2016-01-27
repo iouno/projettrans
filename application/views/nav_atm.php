de1d8674d3415171f84657e5b0a73d83adb3c185
lafranli <lucie.lafrance@etudiant.univ-rennes1.fr>
2016-01-27 17:14:30 +0100

    Update nav_atm.php

--- application/views/nav_atm.php
+++ application/views/nav_atm.php
@@ -1,7 +1,7 @@
 <?php require "localization.php"; ?>
 <nav>
 	<ul>
-		<li><a><?php echo_("Inscriptions");?></a></li>
-		<li><?php echo anchor('atm/traiter_reservation',echo_("Réservations")); ?></li>
+		<li><a><?php echo gettext("Inscriptions");?></a></li>
+		<li><?php echo anchor('atm/traiter_reservation',echo gettext("Réservations")); ?></li>
 	</ul>
 </nav>

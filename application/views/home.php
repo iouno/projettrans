f0432d647153a46f4b9bc65660f47aeeaa789879
lafranli <lucie.lafrance@etudiant.univ-rennes1.fr>
2016-01-27 17:13:58 +0100

    Update home.php

--- application/views/home.php
+++ application/views/home.php
@@ -1,21 +1,21 @@
 <?php require "localization.php"; ?>
 
 
-<p><?php echo anchor('artiste/inscription',echo_("Inscrivez-vous")) ?></p>
-<p><?php echo anchor('artiste/recherche',echo_("Faire une recherche")) ?>(temp)</p>
+<p><?php echo anchor('artiste/inscription',echo gettext("Inscrivez-vous")) ?></p>
+<p><?php echo anchor('artiste/recherche',echo gettext("Faire une recherche")) ?>(temp)</p>
 
 =======
-<?php echo_("Vous êtes déconnecté.");?>
+<?php echo gettext("Vous êtes déconnecté.");?>
 
-<p><?php echo anchor('sessions/inscription',echo_("Inscrivez-vous")) ?></p>
+<p><?php echo anchor('sessions/inscription',echo gettext("Inscrivez-vous")) ?></p>
 
 =======
-<h1><?php echo_("Réserver vos salles maintenant");?></h1>
+<h1><?php echo gettext("Réserver vos salles maintenant");?></h1>
 <p>
-<?php echo_("Trouver les scènes qui répondent au mieux à vos critères et les réserver ? Rien de plus facile ! Artiste ou groupe des Trans Musicales, inscrivez-vous et choisissez !");?>
+<?php echo gettext("Trouver les scènes qui répondent au mieux à vos critères et les réserver ? Rien de plus facile ! Artiste ou groupe des Trans Musicales, inscrivez-vous et choisissez !");?>
 </p>
 <div id="inscription">
-	<?php echo anchor('sessions/inscription',echo_("Inscrivez-vous")) ?>
+	<?php echo anchor('sessions/inscription',echo gettext("Inscrivez-vous")) ?>
 </div>
 
 

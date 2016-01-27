5a83dfca0ea98ce515afe1cb8d2834f6a2096583
lafranli <lucie.lafrance@etudiant.univ-rennes1.fr>
2016-01-27 14:26:52 +0100

    Update connexion.php
    modifications pour la mise en place de la traduction

--- application/views/connexion.php
+++ application/views/connexion.php
@@ -1,17 +1,17 @@
 <h1>Connexion</h1>
 <form method="post" action="connexion">
-	<p class="erreur"><?php echo $msg_erreur ?></p>
+	<p class="erreur"><?php echo_($msg_erreur); ?></p>
 	<p>
-		<label><?php echo "Identifiant" ; ?></label><br />
+		<label><?php echo_("Identifiant") ; ?></label><br />
 		<input name="login" type="text"/>
 	</p>
 	<p>
-		<label><?php echo "Mot de passe"; ?></label><br />
+		<label><?php echo_("Mot de passe"); ?></label><br />
 		<input name="pass" type="password"/>
 	</p>
 	<p>
 		<input type="submit" value="Connexion" class="button" />
 	</p>
 </form>
-<?php echo anchor('sessions/inscription',<?php echo "Pas encore inscrit ?";?>) ?>
+<?php echo anchor('sessions/inscription',echo_("Pas encore inscrit ?")) ?>
 

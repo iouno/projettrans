820d0c58bf8d579c42466d1e7c01f9077725dd7c
lafranli <lucie.lafrance@etudiant.univ-rennes1.fr>
2016-01-27 15:03:04 +0100

    Update registerOk.php

--- application/views/registerOk.php
+++ application/views/registerOk.php
@@ -1,4 +1,4 @@
-<?php require "..\..\localization.php"; ?>
+<?php require "../../localization.php"; ?>
 <h1><?php echo_("Inscription"); ?></h1>
 
 <p> <?php echo_("Votre demande d'inscription a été prise en compte. Un identifiant et un mot de passe vous serons envoyés par courriel lorsque votre inscription sera validée."); ?></p>

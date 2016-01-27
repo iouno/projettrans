dcbdd6b750029b524d2dd3258d815d361cc3a6ee
lafranli <lucie.lafrance@etudiant.univ-rennes1.fr>
2016-01-27 14:29:53 +0100

    Update home.php
    modifications pour la mise en place de la traduction

--- application/views/home.php
+++ application/views/home.php
@@ -1,20 +1,20 @@
 
 
-<p><?php echo anchor('artiste/inscription',"Inscrivez-vous") ?></p>
-<p><?php echo anchor('artiste/recherche',"Faire une recherche") ?>(temp)</p>
+<p><?php echo anchor('artiste/inscription',echo_("Inscrivez-vous")) ?></p>
+<p><?php echo anchor('artiste/recherche',echo_("Faire une recherche")) ?>(temp)</p>
 
 =======
-<?php echo "Vous êtes déconnecté.";?>
+<?php echo_("Vous êtes déconnecté.");?>
 
-<p><?php echo anchor('sessions/inscription', "Inscrivez-vous") ?></p>
+<p><?php echo anchor('sessions/inscription',echo_("Inscrivez-vous")) ?></p>
 
 =======
-<h1><?php echo "Réserver vos salles maintenant";?></h1>
+<h1><?php echo_("Réserver vos salles maintenant");?></h1>
 <p>
-<?php echo "Trouver les scènes qui répondent au mieux à vos critères et les réserver ? Rien de plus facile ! Artiste ou groupe des Trans Musicales, inscrivez-vous et choisissez !";?>
+<?php echo_("Trouver les scènes qui répondent au mieux à vos critères et les réserver ? Rien de plus facile ! Artiste ou groupe des Trans Musicales, inscrivez-vous et choisissez !");?>
 </p>
 <div id="inscription">
-	<?php echo anchor('sessions/inscription', "Inscrivez-vous") ?>
+	<?php echo anchor('sessions/inscription',echo_("Inscrivez-vous")) ?>
 </div>
 
 

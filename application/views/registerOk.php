fb362a1d5e7a6f9ef92bdbed4dec69b3b8928d40
lafranli <lucie.lafrance@etudiant.univ-rennes1.fr>
2016-01-27 14:39:09 +0100

    Update registerOk.php
    modifications pour la mise en place de la traduction

--- application/views/registerOk.php
+++ application/views/registerOk.php
@@ -1,3 +1,3 @@
-<h1><?php echo "Inscription"; ?></h1>
+<h1><?php echo_("Inscription"); ?></h1>
 
-<p> <?php echo "Votre demande d'inscription a été prise en compte. Un identifiant et un mot de passe vous serons envoyés par courriel lorsque votre inscription sera validée."; ?></p>
+<p> <?php echo_("Votre demande d'inscription a été prise en compte. Un identifiant et un mot de passe vous serons envoyés par courriel lorsque votre inscription sera validée."); ?></p>

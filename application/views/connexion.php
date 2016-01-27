d343ed905a2a9042e11cf0bccca59d5d4b80cee0
lafranli <lucie.lafrance@etudiant.univ-rennes1.fr>
2016-01-27 17:12:43 +0100

    Update connexion.php

--- application/views/connexion.php
+++ application/views/connexion.php
@@ -2,18 +2,18 @@
 
 <h1>Connexion</h1>
 <form method="post" action="connexion">
-	<p class="erreur"><?php echo_($msg_erreur); ?></p>
+	<p class="erreur"><?php echo gettext($msg_erreur); ?></p>
 	<p>
-		<label><?php echo_("Identifiant") ; ?></label><br />
+		<label><?php echo gettext("Identifiant") ; ?></label><br />
 		<input name="login" type="text"/>
 	</p>
 	<p>
-		<label><?php echo_("Mot de passe"); ?></label><br />
+		<label><?php echo gettext("Mot de passe"); ?></label><br />
 		<input name="pass" type="password"/>
 	</p>
 	<p>
 		<input type="submit" value="Connexion" class="button" />
 	</p>
 </form>
-<?php echo anchor('sessions/inscription',echo_("Pas encore inscrit ?")) ?>
+<?php echo anchor('sessions/inscription',echo gettext("Pas encore inscrit ?")) ?>
 

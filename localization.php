57658159e459e296bcecad241c0e43c669ffebd6
lafranli <lucie.lafrance@etudiant.univ-rennes1.fr>
2016-01-29 09:54:10 +0100

    Update localization.php

--- localization.php
+++ localization.php
@@ -16,7 +16,7 @@ $locale = (isset($_GET['lang']))? $_GET['lang'] : DEFAULT_LOCALE;
 // gettext setup
 T_setlocale(LC_MESSAGES, $locale);
 // Set the text domain as 'messages'
-$domain = 'en_US'; // ajouter à la fin de l'URL lang=en_US
+$domain = 'trad'; // ajouter à la fin de l'URL lang=en_US
 bindtextdomain($domain, LOCALE_DIR);
 // bind_textdomain_codeset is supported only in PHP 4.2.0+
 if (function_exists('bind_textdomain_codeset')) 
